3382c94e955d7859a8d3bc3707d1f2a53bc49cf1
Assaad Al-Assaad <S01180823@acad.tri-c.edu>
2019-10-17 15:14:02 -0400

    Lab 7

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -10,6 +10,34 @@ namespace Lab7
     {
         static void Main(string[] args)
         {
+            //Part 1 
+            int x = 10;
+
+            do
+            {
+                Console.WriteLine(x);
+                x = x + 10;
+            } while (x <= 100);
+            
+
+            //Part 2
+            int sum = 0;
+            for (int num = 1; num <= 20;num++) 
+            {
+                sum=num + sum;
+            }
+            Console.WriteLine("The sum is" + sum);
+            Console.ReadKey();
+
+            //Part 3
+            int x = 1;
+            for (int x = 1; x<=1; x )
+            {
+
+            }    
+
+
+
         }
     }
 }
